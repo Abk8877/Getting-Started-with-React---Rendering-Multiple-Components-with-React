4b607c232eeca8f38027207e19db1461d846372b
Abhishek Kumar <80087524+Abk8877@users.noreply.github.com>
2021-05-05 22:18:25 +0530

    dremProjectUpdated
    name and description of the dream project of added.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1>Easy Peasy</h1>
+      <p>
+        Easy Peasy is a Cryptocurency exchange platform. It will provide a
+        simple user friendly user interface. It will help people to experiance
+        power of of Crypto Community and participate with maximum ease.{" "}
+      </p>
+    </div>
+  );
+};
 
 export default App;
