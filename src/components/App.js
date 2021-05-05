af18bd7d63222376ac1c0f49b3e95b8bba58c382
Abhishek Kumar <80087524+Abk8877@users.noreply.github.com>
2021-05-05 22:19:41 +0530

    update2

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const App = () => {
       <p>
         Easy Peasy is a Cryptocurency exchange platform. It will provide a
         simple user friendly user interface. It will help people to experiance
-        power of of Crypto Community and participate with maximum ease.{" "}
+        power of of Crypto Community and participate with maximum ease.
       </p>
     </div>
   );
