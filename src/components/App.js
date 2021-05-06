09758013ec5ad4e992f13c64fd6514ce4ff1847d
Abhishek Kumar <80087524+Abk8877@users.noreply.github.com>
2021-05-06 11:40:31 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,9 @@ const App = () => {
     <div id="main">
       <h1 data-ns-test="project-name">Easy Peasy</h1>
       <p data-ns-test="project-description">
-        Easy Peasy is a Cryptocurency exchange platform. It will provide a
-        simple user friendly user interface. It will help people to experiance
-        power of of Crypto Community and participate with maximum ease.
+        Easy Peasy is a Cryptocurency exchange platform. It provides a simple
+        user friendly user interface. It will help people to experiance power of
+        of Crypto Community and participate with maximum ease.
       </p>
     </div>
   );
